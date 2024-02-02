9f7ea5ffd44dc3b22bc1653c5adfa952460f7dc5
Melvin2306 <melvin.rinkleff@gmx.de>
2024-02-02 14:42:25 +0100

    added checks and input for pihole password

--- setup.sh
+++ setup.sh
@@ -2,85 +2,80 @@
 
 # Update package lists
 echo "Updating package lists..."
-sudo apt update
-echo "Package lists updated."
+if sudo apt update; then
+    echo "Package lists updated."
+else
+    echo "Error updating package lists."
+    exit 1
+fi
 
 # Upgrade installed packages
 echo "Upgrading installed packages..."
-sudo apt upgrade -y
-echo "Installed packages upgraded."
+if sudo apt upgrade -y; then
+    echo "Installed packages upgraded."
+else
+    echo "Error upgrading installed packages."
+    exit 1
+fi
 
 # Install Docker Compose
 echo "Installing Docker Compose..."
-sudo apt install docker-compose -y
-echo "Docker Compose installed."
+if sudo apt install docker-compose -y; then
+    echo "Docker Compose installed."
+else
+    echo "Error installing Docker Compose."
+    exit 1
+fi
 
 # Create directories
 echo "Creating directories..."
 cd ..
-mkdir -p nas nextcloud pihole docker cloudflare jellyfin change-detection
-echo "Directories created."
+if mkdir -p nas nextcloud pihole docker cloudflare jellyfin change-detection; then
+    echo "Directories created."
+else
+    echo "Error creating directories."
+    exit 1
+fi
 
-# # Mount NAS
-# echo "Mounting NAS..."
+# Prompt user for Jellyfin WEBPASSWORD
+echo -n "Enter the password for Jellyfin Web interface and press [ENTER]: "
+read WEBPASSWORD
 
-# # Find the device name of the first USB device
-# USB_DEVICE=$(lsblk -o NAME,MOUNTPOINT | grep -v "boot\|root" | awk '/\/media\//{print $1}' | head -n 1)
-
-# if [ -z "$USB_DEVICE" ]; then
-#     echo "No USB device detected."
-#     exit 1
-# fi
-
-# # The full path of the device
-# USB_DEVICE_PATH="/dev/$USB_DEVICE"
-
-# # Directory to mount the USB device
-# MOUNT_DIR="$HOME/nas"
-
-# # Check if the mount directory exists
-# if [ ! -d "$MOUNT_DIR" ]; then
-#     echo "Mount directory $MOUNT_DIR does not exist, creating it..."
-#     mkdir -p "$MOUNT_DIR"
-# fi
-
-# # Mount the USB device
-# echo "Mounting $USB_DEVICE_PATH to $MOUNT_DIR..."
-# sudo mount $USB_DEVICE_PATH $MOUNT_DIR
-
-# if [ $? -eq 0 ]; then
-#     echo "Successfully mounted $USB_DEVICE_PATH to $MOUNT_DIR."
-# else
-#     echo "Failed to mount $USB_DEVICE_PATH."
-# fi
+# Check if WEBPASSWORD is empty
+if [ -z "$WEBPASSWORD" ]; then
+    echo "WEBPASSWORD cannot be empty."
+    exit 1
+fi
 
 # Move docker-compose files to directories
 echo "Moving docker-compose files to directories..."
-mv home-server-setup/pihole/docker-compose.yml pihole/
-rm home-server-setup/pihole
-mv home-server-setup/jellyfin/docker-compose.yml jellyfin/
-rm home-server-setup/jellyfin
-mv home-server-setup/change-detection/docker-compose.yml change-detection/
-rm home-server-setup/change-detection
-echo "Docker-compose files moved."
+if mv home-server-setup/pihole/docker-compose.yml pihole/ && \
+   rm -r home-server-setup/pihole && \
+   mv home-server-setup/jellyfin/docker-compose.yml jellyfin/ && \
+   sed -i "s/WEBPASSWORD: \"pihole\"/WEBPASSWORD: \"$WEBPASSWORD\"/g" jellyfin/docker-compose.yml && \
+   rm -r home-server-setup/jellyfin && \
+   mv home-server-setup/change-detection/docker-compose.yml change-detection/ && \
+   rm -r home-server-setup/change-detection; then
+    echo "Docker-compose files moved and Jellyfin password set."
+else
+    echo "Error moving Docker-compose files or setting Jellyfin password."
+    exit 1
+fi
 
 # Execute docker-compose files
 echo "Executing docker-compose files..."
-cd pihole
-sudo docker-compose up -d
-cd ..
-cd jellyfin
-sudo docker-compose up -d
-cd ..
-cd change-detection
-sudo docker-compose up -d
-cd ..
+cd pihole && sudo docker-compose up -d && cd ..
+cd jellyfin && sudo docker-compose up -d && cd ..
+cd change-detection && sudo docker-compose up -d && cd ..
 echo "Docker-compose files executed."
 
 # Check if docker-compose files are running
 echo "Checking if docker-compose files are running..."
-sudo docker ps
-echo "Docker-compose files are running."
+if sudo docker ps; then
+    echo "Docker-compose files are running."
+else
+    echo "Error checking Docker-compose files."
+fi
 
-# Reboot system
+# Ending message
 echo "Setup Script execution completed successfully!"
